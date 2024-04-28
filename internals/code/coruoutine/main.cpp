91decc063f0038294a0e04766305ab6e7c4f36d1
zav <zav.shotan@gmail.com>
2024-04-28 16:47:49 -0400

    updated

--- internals/code/coruoutine/main.cpp
+++ internals/code/coruoutine/main.cpp
@@ -1,27 +1,46 @@
+#include <coroutine>
 
-class BasicCoroutine
+using namespace std;
+
+struct CoroFrame
 {
-public:
-    struct Promise
-    {
-        BasicCoroutine get_return_object() { return BasicCoroutine{}; }
-
-        void unhandled_exception() noexcept {}
-
-        void return_void() noexcept {}
-
-        std::suspend_never initial_suspend() noexcept { return {}; }
-        std::suspend_never final_suspend() noexcept { return {}; }
-    };
-    using promise_type = Promise;
+    Task<int>::promise_type promise;
+    bool initial_await_resume_called = false;
+    int state = 0;
 };
 
-BasicCoroutine coro()
+int coro_func(CoroFrame &coro)
 {
-    co_return;
+    while (true)
+    {
+        switch (coro.state)
+        {
+        case 0:
+            // Do something
+            coro.state = 1;
+            coro.promise.yield_value(42);
+            await coro.promise.initial_suspend();
+        case 1:
+            // Do something else
+            coro.state = 2;
+            coro.promise.yield_value(84);
+            await coro.promise.final_suspend();
+        case 2:
+            // Done
+            coro.promise.return_value(126);
+            return 126;
+        }
+    }
 }
 
 int main()
 {
-    coro();
+    CoroFrame coro;
+    coro_func(coro);
+
+    int result = coro.promise.get_return_object().get_result();
+
+    cout << result << endl;
+
+    return 0;
 }
