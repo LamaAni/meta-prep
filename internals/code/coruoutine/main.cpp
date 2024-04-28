7adc8e7366caa45d45ce81ca9092c77a69dca73b
zav <zav.shotan@gmail.com>
2024-04-28 16:17:34 -0400

    updated

---
+++ internals/code/coruoutine/main.cpp
@@ -0,0 +1,27 @@
+
+class BasicCoroutine
+{
+public:
+    struct Promise
+    {
+        BasicCoroutine get_return_object() { return BasicCoroutine{}; }
+
+        void unhandled_exception() noexcept {}
+
+        void return_void() noexcept {}
+
+        std::suspend_never initial_suspend() noexcept { return {}; }
+        std::suspend_never final_suspend() noexcept { return {}; }
+    };
+    using promise_type = Promise;
+};
+
+BasicCoroutine coro()
+{
+    co_return;
+}
+
+int main()
+{
+    coro();
+}
